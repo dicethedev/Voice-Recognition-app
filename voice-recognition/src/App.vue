ded61c83988c6b1a602cd95dc724b4d0a91bd01c
dicetheDev <samuelblessing125@gmail.com>
2022-03-24 12:56:17 +0100

    function is now added to script file for more functionality & I style transcript section

--- voice-recognition/src/App.vue
+++ voice-recognition/src/App.vue
@@ -14,18 +14,71 @@ onMounted(() => {
   sr.continous = true;
   sr.interimResults = true;
 
-  sr.onStart = () => {
-    console.log("Speech has started");
+  sr.onstart = () => {
+    console.log("SR has started");
     isRecording.value = true;
   };
-  sr.start();
+
+  sr.onend = () => {
+    console.log("SR has stopped");
+    isRecording.value = false;
+  };
+
+  // sr.start();
+
+  // setTimeout(() => {
+  //   sr.stop();
+  // }, 1000);
+
+  //The Transcript(What you will say is going to print it out)
+  sr.onresult = (event) => {
+    // console.log(event.results);
+    for (let i = 0; i < event.results.length; i++) {
+      const result = event.results[i];
+
+      if (result.isFinal) CheckForCommand(result);
+      //the isFinal is the respond you are going to say to tell the computer to stop either stop recording
+    }
+
+    const trans = Array.from(event.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      // The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string.
+      .join("");
+
+    transcript.value = trans;
+  };
 });
+
+const CheckForCommand = (result) => {
+  const trans = result[0].transcript;
+  if (trans.includes("stop recording")) {
+    sr.stop();
+  } else if (
+    trans.includes("what is the time?") || // this is an "or" sign
+    trans.includes("what's the time")
+  ) {
+    sr.stop();
+    //this is the code implementation to print out the time formart
+    alert(new Date().toLocaleTimeString());
+    //Timeout is here
+    setTimeout(() => sr.start(), 1000);
+  }
+};
+
+const ToggleMic = () => {
+  if (isRecording.value) {
+    sr.stop();
+  } else {
+    sr.start();
+  }
+};
 </script>
 
 <template>
   <div class="app">
-    <button :class="`speak-button`">Record</button>
-    <div class="transcript"></div>
+    <button :class="`speak-button`" @click="ToggleMic">Record</button>
+    <div class="transcript" v-text="transcript"></div>
   </div>
 </template>
 
@@ -58,5 +111,11 @@ body {
   height: 50px;
   font-weight: 400;
   box-shadow: 1px 2px 20px #00cffd;
+}
+.transcript {
+  position: absolute;
+  left: 50%;
+  top: 40%;
+  transform: translate(-50%, -40%);
 }
 </style>
